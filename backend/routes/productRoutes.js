c492d8b4052b9c04011d684c8d94edf3cbf242bf
murkhunting <marcmp.arq@gmail.com>
2021-01-25 17:03:11 +0100

    incial backend

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const { getAllProducts, getProductById } = require("../controller/productControllers");
+
+//GET all products from db - GET /api/products
+router.get("/", getAllProducts)
+
+//GET single product from db - GET /api/products/:id
+router.get("/:id", getProductById)
+
+
+module.exports = router;
