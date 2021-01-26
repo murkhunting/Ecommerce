b2db962493dd477389958205f61d86600cff42c3
murkhunting <marcmp.arq@gmail.com>
2021-01-26 11:29:02 +0100

    create screens and components files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="app">
-      
-    </div>
+    <Router>
+        {/* Navbar */}
+        {/* SideDrawer */}
+        {/* Backdrop */}
+        <main>
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route exact path="/product/:id" component={ProductScreen}/>
+            <Route exact path="/cart" component={CartScreen}/>
+          </Switch>
+        </main>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Navbar.css";
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* logo */}
+            <div className="navbar-logo">
+                <h2>ECOMMERCE</h2>
+            </div>
+
+            {/* links */}
+            <ul className="navbar-links">
+                <li>
+                    <Link to="/cart">
+
+                    </Link>
+                </li>
+            </ul>
+
+            {/* hamburger menu */}
+        </nav>
+    )
+}
+
+export default Navbar
