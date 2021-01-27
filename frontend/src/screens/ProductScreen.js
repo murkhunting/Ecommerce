646419554c5c52c70f627605ddcdc56472dbd699
murkhunting <marcmp.arq@gmail.com>
2021-01-27 12:25:24 +0100

    productscreen style

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,16 +6,16 @@ const ProductScreen = () => {
         <div className="productscreen">
             <div className="product-left">
                 <div className="left-image">
-                    <img src="" alt=""/>
+                    <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt=""/>
                 </div>
                 <div className="left-info">
                     <p className="left-Name">PlayStation 5</p>
                     <p>Price: 499.99€</p>
-                    <p>Description:  </p>
+                    <p>Description: jnbfs iasndf sidnfosind sdijnsdnfskfmn fnsdk dsnfosfn fsdnkfsmdfkdnmfns osdnfosnf </p>
                 </div>
             </div>
             <div className="product-right">
-                <div className="rigth-info">
+                <div className="right-info">
                     <p>
                         Price: <span>499.99€</span>
                     </p>
