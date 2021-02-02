a56dda41de8ccaddb32bd8e04a343f7ce094288c
murkhunting <marcmp.arq@gmail.com>
2021-02-02 11:55:32 +0100

    productscreen finished

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -37,7 +37,7 @@ const ProductScreen = ({ match, history }) => {
               <img src={product.imageUrl} alt={product.name} />
             </div>
             <div className="left-info">
-              <p className="left-name">PlayStation 5</p>
+              <p className="left-name">{product.name}</p>
               <p>Price: ${product.price}</p>
               <p>Description: {product.description}</p>
             </div>
