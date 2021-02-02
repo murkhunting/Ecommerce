5e917698c3617e817075c2ebe9c59e5f7bc5aff0
murkhunting <marcmp.arq@gmail.com>
2021-02-02 14:04:28 +0100

    save cart in localstorage and set as initial value

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -15,8 +15,18 @@ const reducer = combineReducers({
 //for async requests
 const middleware = [thunk];
 
+//set initials values for the cart items from the local storage
+const cartFromLocalStorage = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
+
+const INITIAL_STATE = {
+    cart: {
+        cartItems: cartFromLocalStorage
+    }
+}
+
 const store = createStore(
     reducer,
+    INITIAL_STATE,
     composeWithDevTools(applyMiddleware(...middleware))
 )
 
