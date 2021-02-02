c9774481c2a5ed1c531ff1b0ad47faa9a0971d05
murkhunting <marcmp.arq@gmail.com>
2021-02-02 11:52:20 +0100

    productscreen & addtocart logic

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,55 +1,80 @@
-import React from 'react';
-import "./ProductScreen.css"
-import {useState, useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import "./ProductScreen.css";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-//Actions
+// Actions
 import { getProductDetails } from "../redux/actions/productActions";
-import { addToCart} from "../redux/actions/cartActions";
+import { addToCart } from "../redux/actions/cartActions";
 
-const ProductScreen = (match, history) => {
+const ProductScreen = ({ match, history }) => {
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
 
-    const [qty, setQty] = useState(1);
-    const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.getProductDetails);
+  const { loading, error, product } = productDetails;
 
-    const productDetails = useSelector(state => state.getProductDetails);
-    
-    return (
-        <div className="productscreen">
-            <div className="product-left">
-                <div className="left-image">
-                    <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt=""/>
-                </div>
-                <div className="left-info">
-                    <p className="left-Name">PlayStation 5</p>
-                    <p>Price: 499.99€</p>
-                    <p>Description: jnbfs iasndf sidnfosind sdijnsdnfskfmn fnsdk dsnfosfn fsdnkfsmdfkdnmfns osdnfosnf </p>
-                </div>
+  useEffect(() => {
+    if (product && match.params.id !== product._id) {
+      dispatch(getProductDetails(match.params.id));
+    }
+  }, [dispatch, match, product]);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart(product._id, qty));
+    history.push(`/cart`);
+  };
+
+  return (
+    <div className="productscreen">
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <>
+          <div className="product-left">
+            <div className="left-image">
+              <img src={product.imageUrl} alt={product.name} />
             </div>
-            <div className="product-right">
-                <div className="right-info">
-                    <p>
-                        Price: <span>499.99€</span>
-                    </p>
-                    <p>
-                        Status: <span>In Stock</span>
-                    </p>
-                    <p>
-                        Quantity: 
-                        <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </select>
-                    </p>
-                    <p>
-                        <button type="button">Add to cart</button>
-                    </p>
-                </div>
+            <div className="left-info">
+              <p className="left-name">PlayStation 5</p>
+              <p>Price: ${product.price}</p>
+              <p>Description: {product.description}</p>
             </div>
-        </div>
-    )
-}
+          </div>
+          <div className="product-right">
+            <div className="right-info">
+              <p>
+                Price:
+                <span>${product.price}</span>
+              </p>
+              <p>
+                Status:
+                <span>
+                  {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                </span>
+              </p>
+              <p>
+                Qty
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>
+                  ))}
+                </select>
+              </p>
+              <p>
+                <button type="button" onClick={addToCartHandler}>
+                  Add To Cart
+                </button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
