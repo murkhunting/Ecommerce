7c9fc453105ac7dced7ce61661fbf0086f2904db
murkhunting <marcmp.arq@gmail.com>
2021-02-02 13:59:05 +0100

    navbar and sidedrawer cart count

--- frontend/src/components/SideDrawer.js
+++ frontend/src/components/SideDrawer.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import "./SideDrawer.css"
 import {Link} from "react-router-dom"
+import {useSelector} from "react-redux"
 
 const SideDrawer = ({show, click}) => {
     const SideDrawerClass = ["sidedrawer"];
     if (show){
         SideDrawerClass.push("show");
     }
+
+    const cart = useSelector(state => state.cart);
+    const {cartItems} = cart;
+
+    const getCartCount = () => {
+        return cartItems.reduce((qty, item) => qty + Number(item.qty), 0)
+    }
+
     return (
         <div className={SideDrawerClass.join(" ")}>
             <ul className="sidedrawer-links" onClick={click}>
@@ -15,7 +24,7 @@ const SideDrawer = ({show, click}) => {
                         <img src="../images/blackcart.png" alt="c" className="cart-icon"/>
                         <span>
                             Cart
-                            <span className="siderawer-badge">0</span>
+                            <span className="siderawer-badge">{getCartCount()}</span>
                         </span>
                     </Link>
                 </li>
